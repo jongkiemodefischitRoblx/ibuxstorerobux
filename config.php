a08eb611bdf0468640d18383122df3c2415b7ff9
jongkiemodefischitRoblx <jongkiemodefischit@gmail.com>
2025-12-07 18:50:50 +0800

    config.php
    Add configuration settings for email and admin contact.

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$EMAIL_USER = "ibuxstorenetfily@gmail.com"; // Email bisnis
+$EMAIL_PASS = "kvra kcrn tztz vxni";  // App Password Gmail
+$ADMIN_WA = "+60175953518";                // Nomor admin
+$STORE_NAME = "IbuxStore Official";        // Nama tampil di email
+?>
